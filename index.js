40db9e95c5bf56c0763f97cbb58b49ee1d085dba
raaji01 <raajalakshumik18077@it.ssn.edu.in>
2023-11-15 16:24:27 -0600

    god help

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // Set the destination folder where files will be stored
-    cb(null, path.join(__dirname, 'uploads'));
+    cb(null, path.join(__dirname, 'public', 'uploads'));
   },
   filename: function (req, file, cb) {
     // Set the file name to be the original name of the file
@@ -20,11 +20,14 @@ const storage = multer.diskStorage({
 // Create Multer instance with the configured storage
 const upload = multer({ storage: storage });
 
+// Serve static files from the 'public' directory
+app.use('/upload', express.static(path.join(__dirname, 'public')));
+
 // Set up a route for file uploads
 app.post('/upload', upload.array('files'), (req, res) => {
   // 'files' should match the name attribute in your HTML form for file input
   req.files.forEach((f) => {
-    fs.renameSync(`${f.path}`, `public/uploads/${f.originalname}`);
+    fs.renameSync(`${f.path}`, path.join(__dirname, 'public', 'uploads', f.originalname));
   });
 
   // Files are uploaded, you can perform additional actions here if needed
