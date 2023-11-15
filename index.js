134397a6954ad62f63d4eaf6e1486b69a122781e
raaji01 <raajalakshumik18077@it.ssn.edu.in>
2023-11-15 13:50:02 -0600

    fs try

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const multer = require('multer');
-const cors = require('cors');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Set up CORS middleware
-app.use(cors());
 
 // Set up Multer storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // Set the destination folder where files will be stored
-    cb(null, path.join(__dirname, '/uploads'));
+    cb(null, path.join(__dirname, 'uploads'));
   },
   filename: function (req, file, cb) {
     // Set the file name to be the original name of the file
@@ -27,6 +23,9 @@ const upload = multer({ storage: storage });
 // Set up a route for file uploads
 app.post('/upload', upload.array('files'), (req, res) => {
   // 'files' should match the name attribute in your HTML form for file input
+  req.files.forEach((f) => {
+    fs.renameSync(`${f.path}`, `public/uploads/${f.originalname}`);
+  });
 
   // Files are uploaded, you can perform additional actions here if needed
   res.send('Files uploaded successfully!');
