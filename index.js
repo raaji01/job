610b9edef9086fd9b8d5200e64876549b581f7d9
raaji01 <raajalakshumik18077@it.ssn.edu.in>
2023-11-16 16:27:58 -0600

    newwwllyy

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
 const fs = require('fs');
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(express.json());
+const multer = require("multer");
+//app.use(express.static("public"));
+const upload = multer({ dest: "public/uploads/" });
+//app.use(express.static(__dirname + '/UI.html'));
+//console.log("Directory----"+__dirname);
 
-// Set up Multer storage configuration
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Set the destination folder where files will be stored
-    cb(null, path.join(__dirname, 'uploads'));
-  },
-  filename: function (req, file, cb) {
-    // Set the file name to be the original name of the file
-    cb(null, file.originalname);
-  },
-});
 
-// Create Multer instance with the configured storage
-const upload = multer({ storage: storage });
-
-// Set up a route for file uploads
-app.post('/upload', upload.array('files'), (req, res) => {
-  // 'files' should match the name attribute in your HTML form for file input
-  req.files.forEach((f) => {
-    fs.renameSync(`${f.path}`, `public/uploads/${f.originalname}`);
-  });
-
-  // Files are uploaded, you can perform additional actions here if needed
-  res.send('Files uploaded successfully!');
-});
-
-// Set up a simple route to serve an HTML form for testing
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile('/index.html', { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.post("/upload_files", upload.array("files"), uploadFiles);
+
+function uploadFiles(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    req.files.forEach((f) => {
+        fs.renameSync(`${f.path}`, `public/uploads/${f.originalname}`);
+      });
+    res.json({ message: "1" });
+    //console.log("uploadedfiles" +uploadedFiles);
+}
+app.listen(5000, () => {
+    console.log(`Server started...`);
 });
